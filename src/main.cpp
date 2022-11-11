a6435a3fabbad6983711f16dd47c3f8021d47af2
jan lietaer <jan_lietaer@hotmail.com>
2022-11-11 12:16:58 +0100

    EXTRA INFO OP SCHERM + MAAND KARTIER RESET OMDRAAIEN

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,14 @@ using MyData = ParsedData<
     /* FixedValue */ power_delivered,
     /* FixedValue */ power_returned>;
 
-int32_t reedsverbruiktditkwartier;     // hoeveel is reeds verbruik in het huidig kwartier, pas op kan negatief zijn als er zonnepanelen zijn
-uint32_t maandpiek = 2250000;          // gewenste maandelijkse piek  (2500 *900 = 2250000) Watt seconden als default
-int32_t seconde_meterverbruik;         // hoeveel is de laatste seconde verbruikt volgens de slimme meter pas op kan negatief zijn met PV
-uint16_t secondenverinkwartier = 0;    // hoeveel seconden ver zijn we al in het huidig kwartier 0>900
-int16_t maxverbruikperseconde = 12000; // hoeveel kan verbruikt worden per seconde, zonder geschakelde gebruikers (ofwel max vermogen dat door de hoofd zekering kan, ofwel historische piek per seconde? eenheid is Watt seconde)
-boolean boilerAan = false;             // moet de boiler aan of uit ?
-uint8_t maand, kwartier;               // maand  1>12 , kwartier
+int32_t reedsverbruiktditkwartier;        // hoeveel is reeds verbruik in het huidig kwartier, pas op kan negatief zijn als er zonnepanelen zijn
+uint32_t maandpiek = 2250000;             // gewenste maandelijkse piek  (2500 *900 = 2250000) Watt seconden als default
+int32_t seconde_meterverbruik;            // hoeveel is de laatste seconde verbruikt volgens de slimme meter pas op kan negatief zijn met PV
+uint16_t secondenverinkwartier = 0;       // hoeveel seconden ver zijn we al in het huidig kwartier 0>900
+uint16_t secondenverinkwartierSwitch = 0; // hoeveel seconden ver zijn we al in het huidig kwartier 0>900
+int16_t maxverbruikperseconde = 12000;    // hoeveel kan verbruikt worden per seconde, zonder geschakelde gebruikers (ofwel max vermogen dat door de hoofd zekering kan, ofwel historische piek per seconde? eenheid is Watt seconde)
+boolean boilerAan = false;                // moet de boiler aan of uit ?
+uint8_t maand, kwartier;                  // maand  1>12 , kwartier
 String err;
 MyData data;
 
@@ -42,13 +43,9 @@ void setup()
     tft.setCursor(0, 0);
     tft.setTextDatum(MC_DATUM);
     tft.setTextSize(1);
-
     tft.fillScreen(TFT_BLACK);
-
     tft.setRotation(1);
-
     tft.setTextDatum(0);
-
     tft.setTextColor(TFT_WHITE, TFT_BLACK);
 }
 
@@ -59,14 +56,13 @@ void loop()
         if (reader.parse(&data, &err))
         { //            01234567890123
             // 0-0:1.0.0(200830134039S)
-            if (maand != data.timestamp.substring(2, 4).toInt()) // is de maand juist voorbij?  zet het maand piek terugn op 2250000 Wsec
-            {
-                maand = data.timestamp.substring(2, 4).toInt();
-                maandpiek = 2250000;
-                Serial.println("Nieuwemaand ");
-            }
+
             if (kwartier != data.timestamp.substring(8, 10).toInt() / 15) // is het kwartier juist voorbij?
             {
+
+                tft.drawString("Switchtijd Vorig: " + String(secondenverinkwartierSwitch, 1) + " sec ", 1, 80, 4);
+                tft.drawString("Verbruik vorig: " + String(reedsverbruiktditkwartier, 1) + " Ws ", 1, 100, 4);
+
                 kwartier = data.timestamp.substring(8, 10).toInt() / 15;
                 if (reedsverbruiktditkwartier > maandpiek) // in kwartier dat juist voorbij is was het verbruik hoger dan de vorige maand record
                 {
@@ -75,6 +71,13 @@ void loop()
                 Serial.print("kwartiereindverbuiktWs \t");
                 Serial.println(reedsverbruiktditkwartier);
                 reedsverbruiktditkwartier = 0; // zet het kwartier verbruik terug op 0
+                secondenverinkwartierSwitch = 0;
+            }
+            if (maand != data.timestamp.substring(2, 4).toInt()) // is de maand juist voorbij?  zet het maand piek terugn op 2250000 Wsec
+            {
+                maand = data.timestamp.substring(2, 4).toInt();
+                maandpiek = 2250000;
+                Serial.println("Nieuwemaand ");
             }
             secondenverinkwartier = (data.timestamp.substring(8, 10).toInt() - 15 * kwartier) * 60 + data.timestamp.substring(10, 12).toInt();
             reedsverbruiktditkwartier += data.power_delivered + data.power_returned;                           // updaten totaal reeds gebruikt in huidig kwartier PAS OP GAAT MIS ALS DIT BERICHT NIET IEDERE SECONDE KOMT
@@ -86,17 +89,18 @@ void loop()
                 Serial.print(secondenverinkwartier);
                 Serial.print("\tkwartiereindverbuiktWs\t");
                 Serial.println(reedsverbruiktditkwartier);
-                Serial.println("\tuit ");
+                Serial.println("\tUIT");
             }
             else
             {
                 boilerAan = true; // deze boolean met een pin verbinden en deze dan gebruiken voor een  relais of Shelly, Home Assitant,... aan te sturen
                 digitalWrite(LED_BUILDIN, HIGH);
+                secondenverinkwartierSwitch = secondenverinkwartier;
                 Serial.print("secondenverinkwartier \t");
                 Serial.print(secondenverinkwartier);
                 Serial.print("\tkwartiereindverbuiktWs\t");
                 Serial.print(reedsverbruiktditkwartier);
-                Serial.println("\taan ");
+                Serial.println("\tAAN");
             }
 
             //   boilerAan
@@ -113,8 +117,8 @@ void loop()
         reader.clear();
     }
     delay(250);
-    tft.drawString("Tijd; " + String(secondenverinkwartier, 1) + " sec "  , 1, 0, 4);
-    tft.drawString("Verbruik: " + String(reedsverbruiktditkwartier,1) + " Ws ", 1, 20, 4);
+    tft.drawString("Tijd: " + String(secondenverinkwartier, 1) + " sec ", 1, 0, 4);
+    tft.drawString("Verbruik: " + String(reedsverbruiktditkwartier, 1) + " Ws ", 1, 20, 4);
     tft.drawString("Maandpiek: " + String(maandpiek, 1) + " Ws ", 1, 40, 4);
     if (boilerAan == true)
     {

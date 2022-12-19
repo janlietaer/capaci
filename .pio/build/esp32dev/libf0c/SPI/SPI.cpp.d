80d4615aa0dc5afc63d51360e692184e1004bb17
jan lietaer <jan_lietaer@hotmail.com>
2022-12-19 19:56:35 +0100

    erbij

---
+++ .pio/build/esp32dev/libf0c/SPI/SPI.cpp.d
@@ -0,0 +1,5 @@
+.pio/build/esp32dev/libf0c/SPI/SPI.cpp.o: \
+ C:/Users/jan_l/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src/SPI.cpp \
+ C:/Users/jan_l/.platformio/packages/framework-arduinoespressif32/libraries/SPI/src/SPI.h \
+ C:/Users/jan_l/.platformio/packages/framework-arduinoespressif32/variants/esp32/pins_arduino.h \
+ C:/Users/jan_l/.platformio/packages/framework-arduinoespressif32/cores/esp32/esp32-hal-spi.h
